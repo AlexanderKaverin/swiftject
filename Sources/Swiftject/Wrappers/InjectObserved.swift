ffb5fdbffcd51b8837d61583189429d19a7cd850
Alexander Kaverin <a.kaverin@pnn.in.ua>
2022-01-14 21:28:39 +0200

    revert @InjectObserved deletion

---
+++ Sources/Swiftject/Wrappers/InjectObserved.swift
@@ -0,0 +1,29 @@
+//
+//  InjectObserved.swift
+//  Swiftject
+//
+//  Created by Alexander Kaverin on 19.04.2021.
+//
+
+import Foundation
+import SwiftUI
+
+
+@available(iOS 14, *)
+@propertyWrapper public struct InjectObserved<T>: DynamicProperty where T: ObservableObject {
+    
+    @ObservedObject private var dependency: T
+    
+    public var wrappedValue: T {
+        get { return dependency }
+        mutating set { dependency = newValue }
+    }
+    
+    public var projectedValue: ObservedObject<T>.Wrapper {
+        return self.$dependency
+    }
+    
+    public init() {
+        self.dependency = Container.instance.resolve()
+    }
+}
